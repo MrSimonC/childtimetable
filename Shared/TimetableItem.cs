87d691ec492af333297c8a7a639b41c650a18aaf
Simon Crouch <simon.crouch@felinesoft.com>
2021-02-28 23:46:36 +0000

    create first mock data

---
+++ Shared/TimetableItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class TimetableItem
+    {
+        public string Time { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+    }
+}
